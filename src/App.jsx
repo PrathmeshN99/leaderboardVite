fe0d0f65da55c395ca723330ac33670c9be4febc
Prathmesh-Nagpure-OPSNETIC <prathmesh.nagpure@opsnetic.com>
2024-04-28 17:16:22 +0530

    Started vite

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import BasicTable from "./components/BasicTable";
+import "./App.css";
+import LoginSignup from "./components/LoginSignup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      {/* <Header siteTitle="SLACK SSO" /> */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<BasicTable />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+const Home = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+};
+
+export default App;
